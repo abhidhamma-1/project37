0417e7ac33d23e7aa067ce0e857de81c66c40946
abhidhamma-1 <77876579+abhidhamma-1@users.noreply.github.com>
2021-08-11 12:29:27 +0530

    Add files via upload

---
+++ play.js
@@ -0,0 +1,23 @@
+class play {
+
+play(){
+    //write code here to hide question elements
+}
+play(){
+// write code here to hide question elements
+
+// write code tonchange the background color here
+
+}
+ 
+play(){
+ // write code here to hide question elements
+
+// write code tonchange the background color here
+
+//write code to show a heading for showing the result of quiz
+
+
+}
+
+}
